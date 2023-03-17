030d3066cae0e6f41f0ee231d6f23d4f0b0f90c8
lorenzobasso <lorenzobasso98@gmail.com>
2023-03-17 08:03:41 +0000

    Add text for the UK

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -69,14 +69,16 @@
 		<h3 class="mb-2">Total CO2 equivalent emissions in the UK</h3>
 		<Chart options={UKCO2EqByYear} />
 		<p class="!text-md text-white/75">
-			The graph shows the CO2 emissions from agricultural activities between 1990 and 2022. As can
-			be seen, there has been a steady increase in emissions over the past three decades, with a
-			significant spike in the early 2000s. This increase is likely due to the expansion of the
-			agricultural industry to meet growing demand for food and other agricultural products. While
-			efforts have been made to reduce emissions through more sustainable practices, such as crop
-			rotation and reduced tillage, these measures have not yet been enough to offset the overall
-			growth in agricultural emissions. As we move forward, it will be crucial to continue
-			developing and implementing innovative solutions to reduce these emissions and mitigate their
+			The graph displays the CO2 emissions from agricultural activities in the UK between 1990 and
+			2022. Over this period, there has been a gradual decline in emissions, with occasional
+			fluctuations due to various factors such as weather patterns and changes in land use. This
+			reduction can be attributed to the implementation of various policies and initiatives aimed at
+			promoting more sustainable and environmentally friendly practices in agriculture, such as the
+			use of renewable energy sources and better management of fertilizers and manure. While the UK
+			still faces significant challenges in reducing emissions from agriculture, the positive trend
+			in recent years provides some hope for future progress. Continued efforts to promote
+			sustainable agriculture practices, along with increased public awareness and education, will
+			be crucial in achieving long-term reductions in agricultural emissions and mitigating their
 			impact on the environment.
 		</p>
 	</div>
